1d35c25bd969439954a79b48ed168c924a653494
jasonz666 <jasonz666@qq.com>
2021-05-14 11:22:46 +0800

    显示内容加颜色

--- lesson02/helloos/vgastr.c
+++ lesson02/helloos/vgastr.c
@@ -1,8 +1,25 @@
 // Jason 2021-05-12
+void _strcolor(char* p_strdst, char* string, char color){
+    // a byte ascii + a byte color in VESA display mode
+    *p_strdst++ = *string;
+    *p_strdst = color;
+}
+
 void _strwrite(char* string){
     char* p_strdst = (char*)(0xb8000);
     while (*string){
-        *p_strdst = *string++;
+        if (*string == 'H'){
+            _strcolor(p_strdst, string, (char)0x92);
+        }else if (*string == 'l'){
+            _strcolor(p_strdst, string, (char)0x8c);
+        }else if (*string == 'O'){
+            _strcolor(p_strdst, string, (char)0x85);
+        }else if (*string == 'S'){
+            _strcolor(p_strdst, string, (char)0x8d);
+        }else{
+            *p_strdst = *string;
+        }
+        string++;
         p_strdst += 2;
     }
     return;
--- lesson02/helloos/vgastr.h
+++ lesson02/helloos/vgastr.h
@@ -1,2 +1,4 @@
+// Jason 20210514
+void _strcolor(char* p_strdst, char* string, char color);
 void _strwrite(char* string);
 void printf(char* fmt, ...);
